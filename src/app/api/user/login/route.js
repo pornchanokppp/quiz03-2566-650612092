cf3e5d72862869dd04d3e4c3eb9b7b45156bf13a
pondckaokppp <pondchanokpp2003@gmail.com>
2023-09-14 15:15:01 +0700

    4

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -1,20 +1,31 @@
-import jwt from "jsonwebtoken";
+import jwt, { sign } from "jsonwebtoken";
 
 import { DB, readDB } from "@/app/libs/DB";
 import { NextResponse } from "next/server";
 
 export const POST = async (request) => {
   readDB();
+  const body = await request.json();
+  const { username, password } = body;
+  const user = DB.users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (!user) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Username or Password is incorrect",
+      },
+      { status: 400 }
+    );
+  }
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or Password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
+  const payload = {
+    username,
+    role: user.role,
+  };
 
-  const token = "Replace this with token creation";
+  const token = sign(payload, process.env.JWT_SECRET);
 
   return NextResponse.json({ ok: true, token });
 };
